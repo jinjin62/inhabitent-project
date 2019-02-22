b3700f46a4a573762c84682863ae4806c6021330
Gavin Jin <gavinjin62@gmail.com>
2019-02-22 11:30:08 -0800

    Implemented working businesshours widget

--- plugins/business-hours-widget/views/widget.php
+++ plugins/business-hours-widget/views/widget.php
@@ -1,1 +1,21 @@
 <!-- This file is used to markup the public-facing widget. -->
+<?php if (strlen(trim($monday_friday)) > 0): ?>
+<p>
+    <span class="day-of-week">Monday - Friday:</span>
+    <?php echo $monday_friday; ?>
+</P>
+<?php endif; ?>
+
+<?php if (strlen(trim($saturday)) > 0): ?>
+<p>
+    <span class="day-of-week">Saturda:</span>
+    <?php echo $saturday; ?>
+</P>
+<?php endif; ?>
+
+<?php if (strlen(trim($sunday)) > 0): ?>
+<p>
+    <span class="day-of-week">Sunday:</span>
+    <?php echo $sunday; ?>
+</P>
+<?php endif; ?> 
